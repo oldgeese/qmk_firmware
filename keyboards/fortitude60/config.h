f6628c844b77bdefd64e5a6b89d4f495d6f86509
oldgeese <hiro.onoda@gmail.com>
2021-01-05 11:11:13 +0900

    add IS_COMMAND

--- keyboards/fortitude60/config.h
+++ keyboards/fortitude60/config.h
@@ -21,4 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 #include <serial_config.h>
 
+#define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
+
 #endif  // CONFIG_H
